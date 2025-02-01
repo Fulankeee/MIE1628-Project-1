2267974619db36c78932a9e7ef0f78a40217ef82
Fulankeee <frank823liu@gmail.com>
2025-02-01 16:51:53 -0500

    Update code

--- MapReduce/src/Main.java
+++ MapReduce/src/Main.java
@@ -1,5 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Linecount {
-    public static class Linecountmapper extends Mapper<>
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class lineCount {
+    public static class StubMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        // "LongWritable" Not used in this case but required by Hadoop's Mapper class.
+        private final static IntWritable one = new IntWritable(1);
+        private final static Text line = new Text("LineCount");
+        @Override
+        public void map(LongWritable key, Text value, Context context) {
+            context.write(line, one);
+        }
+    }
 }
